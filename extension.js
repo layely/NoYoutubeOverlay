553a37dcae0e89dbba7f439c7ad296a591370631
layely <ablayelyfondou@gmail.com>
2019-08-05 15:26:04 -0400

    Fix formatting and add an update file.

--- extension.js
+++ extension.js
@@ -1,12 +1,12 @@
 (function() {
-    let styles = `
+  let styles = `
     .ytp-ce-element {
         visibility: hidden !important;
     }
     `;
 
-    let styleSheet = document.createElement("style");
-    styleSheet.type = "text/css";
-    styleSheet.innerText = styles;
-    document.head.appendChild(styleSheet);
+  let styleSheet = document.createElement("style");
+  styleSheet.type = "text/css";
+  styleSheet.innerText = styles;
+  document.head.appendChild(styleSheet);
 })();
